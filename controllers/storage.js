95fee9ac02eaf67bddd164d4d2716bb6114e6dc0
Lautaro Decima <decimalautaro@gmail.com>
2022-05-07 18:25:01 -0300

    creacion controller de storage

--- controllers/storage.js
+++ controllers/storage.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
+const {matchedData} = require('express-validator');
 const {storageModel} = require('../models');
-const PUBLIC_URL= process.env.PUBLIC_URL
+const {handleHttpError} = require('../utils/handleError')
+const fs = require ('fs')
 
+const PUBLIC_URL= process.env.PUBLIC_URL
+const MEDIA_PATH= `${__dirname}/../storage`
 
 
 /** Obtener lista de la base de datos
@@ -9,16 +13,26 @@ const PUBLIC_URL= process.env.PUBLIC_URL
 * @param {*} res
 */
 const getItems = async(req, res) =>{
-    const data= await storageModel.find({})
-    res.send({data})
+    try {
+        const data= await storageModel.find({})
+        res.send({data})
+    } catch (error) {
+        handleHttpError(res, 'ERROR_LIST_ITEMS')
+    }
 }
 
 /**  Obtener un detalle de la base de datos
 * @param {*} req
 * @param {*} res
 */
-const getItem = (req, res) =>{
-
+const getItem = async(req, res) =>{
+    try {
+        const {id} = matchedData(req)
+        const data= await storageModel.findById(id)
+        res.send({data})
+    } catch (error) {
+        handleHttpError(res, 'ERROR_DETAIL_ITEM')
+    }
 }
 
 /** insertar un registro en  la base de datos
@@ -26,31 +40,51 @@ const getItem = (req, res) =>{
 * @param {*} res
 */
 const createItem = async(req, res) =>{
-    const { body, file} = req
-    console.log(file);
-    const fileData = {
-        filename: file.filename,
-        url: `${PUBLIC_URL}/${file.filename}`
+    try {
+        const { body, file} = req
+        console.log(file);
+        const fileData = {
+            filename: file.filename,
+            url: `${PUBLIC_URL}/${file.filename}`
+        }
+        const data= await storageModel.create(fileData)
+        res.send({data})
+    } catch (error) {
+        handleHttpError(res, 'ERROR_CREAR_ITEM')
     }
-    const data= await storageModel.create(fileData)
-    res.send({data})
 }
 
 /**  Actualizar un registro de la base de datos
-* @param {*} req
-* @param {*} res
-*/
-const updateItem = (req, res) =>{
+// * @param {*} req
+// * @param {*} res
+// */
+// const updateItem = async (req, res) =>{
 
-}
+// }
 
 /**  eliminar un registro de la base de datos
 * @param {*} req
 * @param {*} res
 */
-const deleteItem = (req, res) =>{
+const deleteItem = async (req, res) =>{
+    try {
+        const {id} = matchedData(req)
+        const dataFile= await storageModel.findById(id)
+        await storageModel.delete({_id: id}) //delete({_id: id}) para la eliminacion logica
+        const {filename}= dataFile 
+        const filePath = `${MEDIA_PATH}/${filename}`
 
+        // fs.unlinkSync(filePath)  // comentar esa linea si queremos que el borrado sea logico
+
+        const data = {
+            filePath,
+            deleted: 1
+        }
+        res.send({data})
+    } catch (error) {
+        handleHttpError(res, 'ERROR_DETAIL_ITEM')
+    }
 }
 
 
-module.exports = { getItems, getItem, createItem, updateItem, deleteItem }
+module.exports = { getItems, getItem, createItem, deleteItem }
